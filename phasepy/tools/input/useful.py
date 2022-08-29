3c6de2ef226da374ddd80d8ea809a5a4e9c18fbc
SS310 <8217050@ed.tus.ac.jp>
2022-08-29 18:10:42 +0900

    add: add new file
    Added file to summarize functions related to externally available input files

---
+++ phasepy/tools/input/useful.py
@@ -0,0 +1,14 @@
+"""
+Summary
+-------
+
+See Also
+--------
+
+"""
+
+#********** Import major pakage or module **********
+
+#********** Import original module **********
+
+#********** Constant Value **********
