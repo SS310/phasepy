46f84a81e713418a71bf8ef82dc18d3842ba451e
SS310 <8217050@ed.tus.ac.jp>
2022-08-31 02:32:58 +0900

    add: add function for initialization of field variables

--- phasepy/martensite/variable/init/_field.py
+++ phasepy/martensite/variable/init/_field.py
@@ -8,7 +8,28 @@ See Also
 """
 
 #********** Import major pakage or module **********
+import numpy as np
 
 #********** Import orizinal module **********
+from phasepy.martensite.variable.define._var import SimuVal, CellVal
 
 #********** Constant Value **********
+
+class InitField():
+    @staticmethod
+    def toward_center(simu_val: SimuVal, cell_val: CellVal) -> None:
+        # Create random metal structure
+        _Phase.rand(simu_val=simu_val, cell_val=cell_val)
+        print("Random metal structure variable were successfully created. ->", end="")
+
+
+#********** Internal function **********
+
+class _Phase():
+	@staticmethod
+	def rand(simu_val: SimuVal, cell_val: CellVal) -> None:
+		for x in range(simu_val.xmax):
+			for y in range(simu_val.ymax):
+				# Set random value (0~1)
+				cell_val.phase_f[x,y,0] = np.random.rand()
+				cell_val.phase_f[x,y,1] = 1.0 - cell_val.phase_f[x,y,0]
