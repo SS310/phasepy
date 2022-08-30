eb3afb9fc03f69de9c1c1d9be2037a9a2625e54e
SS310 <8217050@ed.tus.ac.jp>
2022-08-31 02:28:40 +0900

    add: add path variable using martensite simulation

--- phasepy/martensite/variable/define/_path.py
+++ phasepy/martensite/variable/define/_path.py
@@ -10,5 +10,30 @@ See Also
 #********** Import major pakage or module **********
 
 #********** Import orizinal module **********
+from phasepy._const import SetPathKey
 
 #********** Constant Value **********
+
+class PathVal():
+    """
+    Variable about PATH
+    """
+    def __init__(self) -> None:
+        """
+        Variable about PATH
+        """
+        self.output_path: str = None
+        """PATH of output directry"""
+        self.input_path: dict = None
+        """PATH of input file"""
+        #self.phase_path: str = None
+        #"""PATH of initial martensite structure image"""
+        
+        self.counter :dict = {
+            SetPathKey.MAIN:
+                {
+                SetPathKey.OUTPUT:False,
+                SetPathKey.INPUT:False
+                }
+        }
+        """Counter of setting"""
