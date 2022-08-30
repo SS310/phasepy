7380dbd6673b587b0c85f49f5cd7e8b06e117c47
SS310 <8217050@ed.tus.ac.jp>
2022-08-31 01:58:27 +0900

    docs: add some comment

--- phasepy/dendrite/main/__init__.py
+++ phasepy/dendrite/main/__init__.py
@@ -1,2 +1,15 @@
+"""
+Summary
+-------
 
+See Also
+--------
+
+"""
+
+#********** Import major pakage or module **********
+
+#********** Import orizinal module **********
 from .round_center import RoundCenter
+
+#********** Constant Value **********
