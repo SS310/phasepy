f175a519096c41a1996d2b8b612f4b69bd804c61
SS310 <8217050@ed.tus.ac.jp>
2022-08-31 02:25:11 +0900

    add: add import-sentence for basic martensite simulation

--- phasepy/martensite/main/__init__.py
+++ phasepy/martensite/main/__init__.py
@@ -10,5 +10,6 @@ See Also
 #********** Import major pakage or module **********
 
 #********** Import orizinal module **********
+from .toward_stable import TowardStable
 
 #********** Constant Value **********
