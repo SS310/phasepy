2e7dea6f5e0f1e873dec0ee98fcdbcf84a89c843
SS310 <8217050@ed.tus.ac.jp>
2022-10-04 12:33:05 +0900

    add: add some array
    These array are used in jitclass.

--- phasepy/tools/_array.py
+++ phasepy/tools/_array.py
@@ -23,7 +23,11 @@ class InitArray():
         self.i4_xy: np.ndarray = np.zeros((self.xmax,self.ymax), dtype="int32")
         self.i4_33: np.ndarray = np.zeros((3,3), dtype="int32")
         self.f4_xy: np.ndarray = np.zeros((self.xmax,self.ymax), dtype="float32")
+        self.c16_xy: np.ndarray = np.zeros((self.xmax,self.ymax), dtype="complex128")
         self.c16_xy2: np.ndarray = np.zeros((self.xmax,self.ymax,2), dtype="complex128")
+        self.c16_xy3: np.ndarray = np.zeros((self.xmax,self.ymax,3), dtype="complex128")
         self.c16_xy33: np.ndarray = np.zeros((self.xmax,self.ymax,3,3), dtype="complex128")
+        self.i4_812: np.ndarray = np.zeros((81,2), dtype="int32")
+        self.i4_814: np.ndarray = np.zeros((81,4), dtype="int32")
         
         
