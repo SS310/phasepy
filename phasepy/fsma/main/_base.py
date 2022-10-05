d5a0178cf40e5b37d6bb1ab42bdb242cece0e76b
SS310 <8217050@ed.tus.ac.jp>
2022-10-05 11:35:53 +0900

    refactor: refactor main routin of fsma simulation

--- phasepy/fsma/main/_base.py
+++ phasepy/fsma/main/_base.py
@@ -11,15 +11,15 @@ See Also
 import sys
 
 #********** Import orizinal module **********
-from phasepy.martensite.variable.define._whole import WholeVal
+from phasepy.fsma.variable.define._whole import WholeVal
 from phasepy.tools._path import PathTools, Counter
 from phasepy._const import SetPathKey, SimulationModelKey
-from phasepy.martensite._const import OutputDir
+from phasepy.fsma._const import OutputDir, FsmaSetPathKey
 from phasepy.tools._save import SaveTools
-from phasepy.martensite.variable.update._strain import Strain
-from phasepy.martensite.variable.update._energy import PhaseEnergy
-from phasepy.martensite.variable.update._field import UpdateField
-from phasepy.martensite.variable.init._field import InitField
+from phasepy.fsma.variable.update._strain import Strain
+from phasepy.fsma.variable.update._energy import PhaseEnergy, MagneEnergy
+from phasepy.fsma.variable.update._field import UpdateField
+from phasepy.fsma.variable.init._field import InitField
 
 #********** Constant Value **********
 
@@ -52,10 +52,10 @@ class Base():
         print("********** Initialization of magnetic domain and metal structure **********")
 
         # Monitoring of initial conditions
-        print("Monitoring of initial conditions -> ", end="")
+        print("Monitoring of initial conditions")
         sys.stdout.flush()
         self.__check_counter()
-        print("Done")
+        print("-----> Done")
 
         # Declaration of Variables(Very heavy because of jit)
         print("Declaration of Variables(Very heavy because of jit) -> ", end="")
@@ -64,10 +64,10 @@ class Base():
         print("Done")
 
         # Initialization of field variables
-        print("Initialization of field variables -> ", end="")
+        print("Initialization of field variables...",)
         sys.stdout.flush()
         self._init_field()
-        print("Done")
+        print("-> Done")
 
         # Saving Initial Conditions
         print("Saving Initial Conditions -> ", end="")
@@ -111,13 +111,53 @@ class Base():
         
         Example
         -------
-        >>> instance.set_output_path(input_path="~./setting.yaml")
+        >>> instance.set_input_path(input_path="~./setting.yaml")
             1) Success
             Nothing happens.
             2) Failure
             FileNotFoundError "< ~./setting.yaml > is not exist."
         """
         PathTools.set_path(path_val=self.__whole_val.path_val, set_path=input_path, key1=SetPathKey.MAIN, key2=SetPathKey.INPUT)
+
+    def set_phase_path(self, phase_path: str) -> None:
+        """
+        Specify PATH to initial metal domain
+
+        Parameter
+        ---------
+        phase_path: str
+            PATH to input file
+            (Relative PATH is also possible, requrie <png> or <npy>)
+        
+        Example
+        -------
+        >>> instance.set_phase_path(phase_path="~./ooo.npy")
+            1) Success
+            Nothing happens.
+            2) Failure
+            FileNotFoundError "< ~./ooo.npy > is not exist."
+        """
+        PathTools.set_path(path_val=self.__whole_val.path_val, set_path=phase_path, key1=SetPathKey.SUB, key2=FsmaSetPathKey.PHASE_PATH)
+
+    def set_magne_path(self, magne_path: str) -> None:
+        """
+        Specify PATH to initial magnetic domain
+
+        Parameter
+        ---------
+        magne_path: str
+            PATH to input file
+            (Relative PATH is also possible, requrie <png> or <npy>)
+        
+        Example
+        -------
+        >>> instance.set_phase_path(magne_path="~./ooo.npy")
+            1) Success
+            Nothing happens.
+            2) Failure
+            FileNotFoundError "< ~./ooo.npy > is not exist."
+        """
+        PathTools.set_path(path_val=self.__whole_val.path_val, set_path=magne_path, key1=SetPathKey.SUB, key2=FsmaSetPathKey.MAGNE_PATH)
 
     def _save(self, step: int) -> None:
         if step == 0:
@@ -135,19 +175,55 @@ class Base():
         Strain.calc_ep_eigen_ave(cell_val=self.__whole_val.cell_val)
 
     def _calc_energy(self) -> None:
+        # ----------------------------------------
+        # Calculate driving force of metal domain
+        # Chemical energy
         PhaseEnergy.Chemical.calc_drive(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Gradient energy
         PhaseEnergy.Gradient.calc_drive(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Elastic Energy
         PhaseEnergy.Elastic.calc_drive(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # ----------------------------------------
+        # Calculate energy of metal domain
+        # Chemical energy
         PhaseEnergy.Chemical.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Gradient energy
         PhaseEnergy.Gradient.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
-        PhaseEnergy.Elastic.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)  
+        # Elastic Energy
+        PhaseEnergy.Elastic.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+
+        # ----------------------------------------
+        # Calculate magnetic field of magnetic domain
+        # Magnetostatic energy
+        MagneEnergy.MagnetoStatic.calc_h(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Magnetoelastic
+        MagneEnergy.MagnetoElastic.calc_h(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Exchange energy
+        MagneEnergy.Exchange.calc_h(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Anisotropy energy
+        MagneEnergy.Anisotropy.calc_h(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # ----------------------------------------
+        # Calculate magnetic field of magnetic domain
+        # Magnetostatic energy
+        MagneEnergy.MagnetoStatic.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Magnetoelastic
+        MagneEnergy.MagnetoElastic.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Exchange energy
+        MagneEnergy.Exchange.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Anisotropy energy
+        MagneEnergy.Anisotropy.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        # Zeeman energy
+        MagneEnergy.Zeeman.calc_energy(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+
 
     def _update_field(self) -> None:
         UpdateField.phase_f(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
+        UpdateField.magne_f(simu_val=self.__whole_val.simu_val, prop_val=self.__whole_val.prop_val, cell_val=self.__whole_val.cell_val)
 
     def _init_field(self) -> None:
-        if self.__class__.__name__ == SimulationModelKey.MARTENSITE.TOWARD_STABLE[SimulationModelKey.CLASS_NAME]:
-            InitField.toward_center(simu_val=self.__whole_val.simu_val, cell_val=self.__whole_val.cell_val)
+        if self.__class__.__name__ == SimulationModelKey.FSMA.TOWARD_STABLE[SimulationModelKey.CLASS_NAME]:
+            InitField.Phase.toward_center(path_val=self.__whole_val.path_val ,simu_val=self.__whole_val.simu_val, cell_val=self.__whole_val.cell_val)
+            InitField.Magne.toward_center(path_val=self.__whole_val.path_val ,simu_val=self.__whole_val.simu_val, cell_val=self.__whole_val.cell_val)
         
         # Strain calculations in the initial field
         self._calc_strain()
@@ -155,7 +231,7 @@ class Base():
         self._calc_energy()
 
     def __check_counter(self) -> None:
-        #Counter.warning(path_val=self.__whole_val.path_val, key1=SetPathKey.MAIN)
+        Counter.warning(path_val=self.__whole_val.path_val, key1=SetPathKey.SUB)
         Counter.error(path_val=self.__whole_val.path_val, key1=SetPathKey.MAIN)
 
     @property
