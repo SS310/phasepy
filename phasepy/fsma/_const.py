d39978f5b8b085285bb4ca37659179a141332c4b
SS310 <8217050@ed.tus.ac.jp>
2022-10-05 11:35:19 +0900

    add: add some constants

--- phasepy/fsma/_const.py
+++ phasepy/fsma/_const.py
@@ -20,8 +20,19 @@ class OutputDir():
     """Variable name for phase field variable"""
     __PHASE_FIELD_IMG = "phase_img"
     """Directry name for image of phase field variable"""
+    __MAGNE_FIELD = "magne_f"
+    """Variable name for magnetic moment"""
+    __MAGNE_FIELD_IMG = "magne_img"
+    """Directry name for image of magnetic moment"""
     OUTIMG_LIST = [
         [__PHASE_FIELD, SaveImgMode.PHASE_TWO, __PHASE_FIELD_IMG],
+        [__MAGNE_FIELD, SaveImgMode.MAGNETIC_MOMENT, __MAGNE_FIELD_IMG]
     ]
     """
     """
+
+class FsmaSetPathKey():
+    PHASE_PATH = "phase_path"
+    """Key of phase-path"""    
+    MAGNE_PATH = "magne_path"
+    """Key of magne-path"""
